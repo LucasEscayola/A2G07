cc0331e87630fc266e37ea065b74e5cc6cb23324
Blademan <albert.blade@alumni.esade.edu>
2024-11-09 22:35:08 +0100

    K: Fixing critical bugs in file1_feature1.py

--- file1_feature1.py
+++ file1_feature1.py
@@ -1,9 +1,15 @@
 def is_number(number):
+    # Check if the input is an integer or a float
     return isinstance(number, int) or isinstance(number, float)
 
 def division(numerator, denominator):
-   if denominator == 1:
+   # Raise an error if the denominator is 0
+   if denominator == 0:
        raise ZeroDivisionError
-   if not is_number(numerator) or not is_number(denominator):
+   # Check if both inputs are numbers, otherwise raise a TypeError 
+   if not is_number(numerator) and not is_number(denominator):
        raise TypeError
+   # Perform the division if inputs are valid
    return numerator/denominator
+
+
