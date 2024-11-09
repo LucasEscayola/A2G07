5c0f58412335decd4861e0168a8e6f28824ee954
victorperez22 <victor.perez7d@alumni.esade.edu>
2024-11-09 23:42:30 +0100

    Adding test_feature1.py to feature1

---
+++ test_feature1.py
@@ -0,0 +1,25 @@
+import unittest
+Import utils
+
+class TestUtils(unittest.TestCase):
+    
+    def test_division_valid(self):
+        self.assertEqual(utils.division(10, 2), 5)
+        self.assertEqual(utils.division(-6, 2), -3)
+    
+    def test_division_zero_division(self):
+        with self.assertRaises(ZeroDivisionError):
+            utils.division(10, 0)
+
+    def test_division_invalid_input(self):
+        with self.assertRaises(TypeError):
+            utils.division("10", 2)
+        with self.assertRaises(TypeError):
+            utils.division(10, "2")
+        with self.assertRaises(TypeError):
+            utils.division("10", "2")
+        with self.assertRaises(TypeError):
+            utils.division([], "2")
+        with self.assertRaises(TypeError):
+            utils.division("10", {})
+
